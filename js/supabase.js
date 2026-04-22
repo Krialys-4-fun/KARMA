4fbc36acc45dcdb1aade503e4b956e12e0d90a7b
Krialys-4-fun <adeline.mouzard@krialys.com>
2026-04-22 17:54:20 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,6 @@
-/* KARMA */
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
+const SUPABASE_URL = 'https://urjykqfscmpffromuumk.supabase.co';
+const SUPABASE_KEY = 'ta_publishable_key_complète_ici';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
